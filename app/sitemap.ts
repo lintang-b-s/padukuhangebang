3d9c4c1f7843e7319c45429a2d316bd0c8687067
lintangbs2 <lintang.birda.saputra@mail.ugm.ac.id>
2025-08-12 03:16:33 +0700

    add sitemap.ts & robot.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,66 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.padukuhangebang.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.padukuhangebang.com/profil",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.padukuhangebang.com/umkm",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.01,
+    },
+    {
+      url: "https://www.padukuhangebang.com/umkms",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.01,
+    },
+    {
+      url: "https://www.padukuhangebang.com/destinations",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.01,
+    },
+    {
+      url: "https://www.padukuhangebang.com/booklet",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.01,
+    },
+    {
+      url: "https://www.padukuhangebang.com/kebudayaan",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.05,
+    },
+    {
+      url: "https://www.padukuhangebang.com/galeri",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.05,
+    },
+    {
+      url: "https://www.padukuhangebang.com/events",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.05,
+    },
+    {
+      url: "https://www.padukuhangebang.com/artikel",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.3,
+    },
+  ];
+}
